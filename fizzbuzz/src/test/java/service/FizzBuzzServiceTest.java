f182c3492b602e36021f0a388e1c88607eb97c02
FJG-KEI <kei.gds.pmqhk@gmail.com>
2025-10-12 19:12:02 +0900

    Test04をついかしました

--- fizzbuzz/src/test/java/service/FizzBuzzServiceTest.java
+++ fizzbuzz/src/test/java/service/FizzBuzzServiceTest.java
@@ -1,9 +1,9 @@
 package service;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.Description;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzServiceTest {
 
@@ -32,5 +32,14 @@ public class FizzBuzzServiceTest {
     String actual = target.fizzBuzz("3");
 
     assertThat(actual).isEqualTo("fizz");
+  }
+  
+  @Test
+  @Description("4を渡したら4が返ってくること")
+  public void test04() {
+    FizzBuzzService target = new FizzBuzzService();
+    String actual = target.fizzBuzz("4");
+
+    assertThat(actual).isEqualTo("4");
   }
 }
