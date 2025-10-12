f098cc4712b08be2cb111461872b93f93737b52a
FJG-KEI <kei.gds.pmqhk@gmail.com>
2025-10-12 19:33:59 +0900

    実装を修正しました

--- fizzbuzz/src/main/java/service/FizzBuzzService.java
+++ fizzbuzz/src/main/java/service/FizzBuzzService.java
@@ -3,7 +3,7 @@ package service;
 public class FizzBuzzService {
   public String fizzBuzz(String number) {
 	  
-    if (number.equals("3") || number.equals("6")) {
+    if (number.equals("3") || number.equals("6") || number.equals("9")) {
       return "fizz";
     } else if (number.equals("5")) {
       return "buzz";
