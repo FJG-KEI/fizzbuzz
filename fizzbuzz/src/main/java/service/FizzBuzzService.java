f65eb121ca288d2c59585eb1241718f06d673058
FJG-KEI <kei.gds.pmqhk@gmail.com>
2025-10-19 19:17:44 +0900

    実装を修正しました

--- fizzbuzz/src/main/java/service/FizzBuzzService.java
+++ fizzbuzz/src/main/java/service/FizzBuzzService.java
@@ -4,7 +4,7 @@ public class FizzBuzzService {
   public String fizzBuzz(String number) {
     if (Integer.parseInt(number) % 3 == 0) {
       return "fizz";
-    } else if (number.equals("5")) {
+    } else if (Integer.parseInt(number) % 5 == 0) {
       return "buzz";
     }
     return number;
